db510b0292c532a05170e4c6a968e395316e0d3a
devMainak <81987904+Cyborg-CS@users.noreply.github.com>
2022-09-06 13:39:41 +0000

    fix: same price evaluation

--- script.js
+++ script.js
@@ -11,11 +11,15 @@ function calculateProfitAndLoss(e){
   var numberOfStocks = Number(stockQuantity.value);
   var sellingPrice = Number(currentPrice.value);
   
-  if(costPrice !== ''){
-     if(numberOfStocks !== ''){
-         if(sellingPrice !== ''){
+  if(costPrice !== 0 && costPrice > 0){
+     if(numberOfStocks !== 0 && numberOfStocks > 0){
+         if(sellingPrice !== 0 && sellingPrice > 0){
             
-            if(costPrice < sellingPrice){
+           if(costPrice === sellingPrice){
+             resultBox.innerText = "You neither made profit or loss :(";
+           }else{ 
+           
+           if(costPrice < sellingPrice){
                var [profit, profitPercentage] = calculateProfit(costPrice, sellingPrice);
                 var netProfit = profit * numberOfStocks;
                 resultBox.innerText = `Wohoo ! you made some profit wolfy... your profit is ${netProfit} and  profit percentage is ${profitPercentage.toFixed(2)}%`;resultBox.style.color = "green";
@@ -25,16 +29,17 @@ function calculateProfitAndLoss(e){
             var netLoss = loss * numberOfStocks;
             resultBox.innerText = `Whoops!! Your loss is ${netLoss} and loss percentage is ${lossPercentage.toFixed(2)}% :(`;
             resultBox.style.color = "red";
-            } 
+            }; 
+           };
 
          }else{
-           resultBox.innerText = "Input the current stock price.";
+           resultBox.innerText = "Input the current stock price. (Positive Number's Only)";
          }
      }else{
-       resultBox.innerText = "Input how many stocks are there.";
+       resultBox.innerText = "Input how many stocks are there. (Positive Number's Only)";
      }
   }else{
-    resultBox.innerText = "Input the initial price.";
+    resultBox.innerText = "Input the initial price. (Positive Number's Only)";
   }
 }
 
